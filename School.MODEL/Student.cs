ed991a29c48e3832a75fdff16d37d656a4276a97
Karol Kiptoo <kiptoocarroll@gmail.com>
2025-01-28 17:16:34 +0300

    studentValidator

--- School.MODEL/Student.cs
+++ School.MODEL/Student.cs
@@ -3,8 +3,8 @@
     public class Student : Base
     {
         public long Id { get; set; }
-        public required string Name { get; set; }
-        public required string RegistrationNumber { get; set; }
+        public  string Name { get; set; }
+        public  string RegistrationNumber { get; set; }
         public DateTime DateOfJoin { get; set; }
         public bool IsActive { get; set; }
         public long HostelId { get; set; }
