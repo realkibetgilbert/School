1212985a5ac09ffd32e3f7662e53cf76ce02b240
Gilbert Kibet <kibetgilly354@gmail.com>
2025-01-28 17:49:43 +0300

    ACXZ

--- School.API/Controllers/HostelController.cs
+++ School.API/Controllers/HostelController.cs
@@ -1,12 +1,7 @@
-﻿using AutoMapper;
-using FluentValidation;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 using School.API.Data;
 using School.API.Dto.Hostel;
-using School.API.Dto.Students;
 using School.API.Interfaces.implementations;
-using School.API.Utils.Validators;
 using School.MODEL;
 
 namespace School.API.Controllers
@@ -15,59 +10,39 @@ namespace School.API.Controllers
     [ApiController]
     public class HostelController : ControllerBase
     {
-       
+        private readonly SchoolDbContext _schoolDbContext;
         private readonly IHostelService _hostelService;
-        private readonly IMapper _mapper;
-        private readonly IValidator<HostelToCreateDto> _validator;
 
-        public HostelController(IHostelService hostelService, IMapper mapper, IValidator<HostelToCreateDto> validator)
+        public HostelController(SchoolDbContext schoolDbContext, IHostelService hostelService)
         {
-           
+            _schoolDbContext = schoolDbContext;
             _hostelService = hostelService;
-            _mapper = mapper;
-            _validator = validator;
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateAsync([FromBody] HostelToCreateDto hostelToCreateDto)
         {
-            var valid = await _validator.ValidateAsync(hostelToCreateDto);
-
-            // Check if validation failed
-            if (!valid.IsValid)
+            var hostel = new Hostel
             {
-                // Extract errors and format them as a list of messages
-                var errors = valid.Errors.Select(e => new
-                {
-                    Field = e.PropertyName,
-                    ErrorMessage = e.ErrorMessage
-                });
-
-                // Return a 400 Bad Request response with the errors
-                return BadRequest(new
-                {
-                    Message = "Validation failed",
-                    Errors = errors
-                });
-            }
-
-            var hostels = _mapper.Map<Hostel>(hostelToCreateDto);
-            return Ok(await _hostelService.CreateAsync(hostels));
+                Name = hostelToCreateDto.Name,
+                Description = hostelToCreateDto.Description,
+                CreatedOn = DateTime.Now,
+                CreatedBy = "system",
+            };
+            return Ok(await _hostelService.CreateAsync(hostel));
         }
 
         [HttpGet]
         [Route("{id:long}")]
         public async Task<IActionResult> GetAsync([FromRoute] long id)
         {
-            var hostel = await _hostelService.GetByIdAsync(id);
-            return Ok(_mapper.Map<HostelToDisplayDto>(hostel));
+            return Ok(await _hostelService.GetByIdAsync(id));
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAsync()
         {
-            List<Hostel> hostel = await _hostelService.GetAllAsync();
-            return Ok(_mapper.Map<List<HostelToDisplayDto>>(hostel));
+            return Ok(await _hostelService.GetAllAsync());
         }
 
         [HttpPut]
